3477ab53146fc995c69bbd5ad12ccbd5386e3c88
PrasannaPattankar <prasanna.pattankar@gmail.com>
2019-04-02 16:21:40 +0530

    Add files via upload
    changes after store name and store selector pop

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Image, Text, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  AsyncStorage
+} from "react-native";
 import Login from "./src/components/Login";
 import Dashboard from "./src/components/Dashboard";
 import Schedule from "./src/components/Schedule";
@@ -8,7 +15,11 @@ import WorkHours from "./src/components/WorkHours";
 import DatewiseReport from "./src/components/DatewiseReport";
 import ScheduleAdd from "./src/components/ScheduleAdd";
 import IndividualReport from "./src/components/IndividualReport";
+import IndividualReportTable from "./src/components/IndividualReportTable";
 import Clock from "./src/components/Clock";
+import Storetableview from "./src/components/Storetableview";
+import Popupmenu from "./src/components/Popupmenu";
+
 import { createStackNavigator, createSwitchNavigator } from "react-navigation";
 import { createAppContainer } from "react-navigation";
 
@@ -20,15 +31,15 @@ export default class App extends Component {
 const AppStackNavigator = createStackNavigator(
   {
     Home_screen: { screen: Login },
-    Dashboard_screen: {
-      screen: Dashboard
-    },
+
     Attendence_screen: {
       screen: Attendence
     },
     Schedule_screen: {
       screen: Schedule
     },
+    Storetableview: { screen: Storetableview },
+    Popupmenu: { screen: Popupmenu },
     WorkHoursReport_screen: {
       screen: WorkHours
     },
@@ -40,6 +51,57 @@ const AppStackNavigator = createStackNavigator(
     },
     Individual_report: {
       screen: IndividualReport
+    },
+    IndividualTable_screen: {
+      screen: IndividualReportTable
+    },
+    Dashboard_screen: {
+      screen: Dashboard,
+      navigationOptions: ({ navigate, navigation }) => ({
+        headerVisible: true,
+        headerLeft: null,
+        headerTitle: (
+          <View style={{ flex: 1, alignItems: "center", marginLeft: 30 }}>
+            <Image
+              source={require("./src/images/poslogo.jpg")}
+              style={{
+                height: "60%",
+                width: "50%",
+                resizeMode: "contain",
+                flex: 1
+              }}
+            />
+          </View>
+        ),
+        headerRight: (
+          <Popupmenu
+            menutext="Menu"
+            menustyle={{
+              marginRight: 16,
+              flexDirection: "row",
+              justifyContent: "flex-end"
+            }}
+            //Menu Text Style
+            textStyle={{
+              color: "white"
+            }}
+            //Click functions for the menu items
+            role={() => {}}
+            barcode={() => {
+              navigation.navigate("BarcodeSettings");
+            }}
+            Store={() => {
+              navigation.navigate("Storetableview");
+            }}
+            logout={() => {
+              //AsyncStorage.clear()
+              AsyncStorage.removeItem("savedPassword");
+
+              navigation.navigate("Loginscreen");
+            }}
+          />
+        )
+      })
     }
   },
   {
